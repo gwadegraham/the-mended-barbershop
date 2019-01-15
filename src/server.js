48d6ef43378b8d402c22bdc7b3375f881d2ed32f
Garrett Graham <gwgraham@uark.edu>
2019-01-14 21:29:24 -0600

    deployment fix

--- src/server.js
+++ src/server.js
@@ -4,12 +4,12 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/break-the-cycle'));
+// Serve only the static files form the angularapp directory
+app.use(express.static(__dirname + '/break-the-cycle'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/break-the-cycle/index.html'));
+
+res.sendFile(path.join(__dirname+'/break-the-cycle/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
